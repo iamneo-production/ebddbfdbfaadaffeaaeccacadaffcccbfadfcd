f9af3ca97621fd09c2002601329fb35301e0ed1a
Iamneo <techadmin@iamneo.ai>
2023-08-10 12:40:52 +0000

    Backup Commit

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -22,12 +22,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ForexPipe } from './forex.pipe';
+import { ForexpipePipe } from './forexpipe.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ForexPipe
+    ForexpipePipe
   ],
   imports: [
     BrowserModule,
---
+++ angularapp/src/app/forexpipe.pipe.ts
@@ -0,0 +1,28 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'forexpipe'
+})
+export class ForexpipePipe implements PipeTransform {
+
+  transform(value: unknown, ...args: unknown[]): unknown {
+    return null;
+  }
+
+}
+
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'forex'
+})
+export class ForexPipe implements PipeTransform {
+  transform(Amount:number,opted:any,opted2:any): any {
+    if (!opted || !opted2) {
+      return Amount;
+    }
+    else {
+    return (Amount *opted2 / opted)}
+  }
+
+}
