ac2d2d6477bfe4925f6e9cb16939abf2550cfa06
Iamneo <techadmin@iamneo.ai>
2023-08-10 13:00:49 +0000

    Backup Commit

---
+++ angularapp/src/app/forex.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ForexPipe } from './forex.pipe';
+
+describe('ForexPipe', () => {
+  it('create an instance', () => {
+    const pipe = new ForexPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
